76da42ca98dade00186fa7541eb07b174b7bf003
KevalMarmik <133678949+KevalMarmik@users.noreply.github.com>
2023-06-08 11:38:43 -0400

    Update terraformfile.tf

--- terraformfile.tf
+++ terraformfile.tf
@@ -15,7 +15,7 @@ provider "aws" {
   region     = "us-east-1"
   access_key = "$AWS_ACCESS_KEY"
   secret_key = "$AWS_SECRET_KEY"
-
+}
 resource "aws_instance" "myec2" {
    ami = "ami-0bef6cc322bfff646"
    instance_type = "t2.micro"
